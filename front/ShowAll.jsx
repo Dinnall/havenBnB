fda305c95e5a55672892aae3ce25d3f828ba5f86
michael <michaeldinnall@gmail.com>
2017-01-21 16:49:41 -0500

    show all listings

---
+++ front/ShowAll.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import {Link} from 'react-router';
+
+const AllListings = React.createClass({
+	getInitialState(){
+		return {places: []}
+	},
+	componentDidMount(){
+		 {
+			$.ajax({
+				url: '/api/listing',
+				type: "GET"
+			})
+			.done( (data) => {
+				this.setState({places: data})
+			})
+		}
+	},
+	render: function(){
+				console.log("CURRENT LISTINGS", this.props)
+			return(
+			<div>
+				<h2>All Available Listings</h2>
+
+				
+					{this.state.places.length=== 0 ? "Loading..." : this.state.places.map((place, idx)=> {
+						return (
+
+								<Link to={"/listing/" + place.id} key={idx}>
+								 <div className="oneList">
+							  		<img  className="gridImg" src={place.images[0]} />
+							  		<p className="guestLimit"><strong>{place.guestLimit}</strong>/per night</p>
+							  		<p className="availabile"><strong>{place.availability}</strong></p>
+									</div>
+								</Link>
+							
+						)
+					})}
+				
+
+			</div>
+		)
+	}
+})
+
+export default AllListings;
