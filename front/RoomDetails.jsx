ff078ff74bcf3e48d6b0f77792dee38035599fbe
Esmeralda R <esmeralda.re@outlook.com>
2017-01-22 15:45:56 -0500

    Add nearby kitchens

--- front/RoomDetails.jsx
+++ front/RoomDetails.jsx
@@ -10,6 +10,7 @@ const RoomDetails = React.createClass({
     })
   },
 
+  //gwtting listing info
   componentDidMount(){
     var id = this.props.params.id;
     $.ajax({
@@ -25,23 +26,31 @@ const RoomDetails = React.createClass({
     this.setState({zipCode:event.target.value});
   },
 
+  //api ajax call
   findSoupKitchens(event){
     event.preventDefault();
     var zipcode = this.state.zipCode;
     $.ajax({
       url:'https://c4q-dot-searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/'+zipcode+'/programs?api_key=b30f1b9f41161c0fb3b39cb49aff8104&serviceTag=food+pantry,free+meals&cursor=0&limit=40'
-    })
+    }) //filtering open kitchens only
     .done((data) => {
-      console.log(data);
       var activeKitchens = [];
+      var kitchenDescription = [];
       data.programs.forEach((val) => {
-        //console.log("val", val.offices[0].open_now_info.open_now);
         if(val.offices[0].open_now_info.open_now === true){
           activeKitchens.push(val);
         }
       })
-      console.log(activeKitchens);
-      this.setState({nearbyFood: activeKitchens});
+        //generating list of open kitchens
+      if(activeKitchens.length !== 0){
+        activeKitchens.forEach((val) => {
+          //console.log(val.offices[0]);
+          kitchenDescription.push(<li><h5>{val.name}</h5></li>, <li>Address: {val.offices[0].address1}</li>, <li>City: {val.offices[0].city}</li>, <li>Zip Code: {val.offices[0].postal}</li>, <li>Phone Number: {val.offices[0].phone_number}</li>)
+
+      this.setState({nearbyFood: kitchenDescription});
+        })
+    }
+
     })
     .catch((error) => {
       console.log(error);
@@ -55,8 +64,9 @@ const RoomDetails = React.createClass({
       <div>
         <div>
           <img src={this.state.roomInfo[0].images[0]} />
-          <p>What to expect: {this.state.roomInfo[0].description}</p>
-          <p>Guests per night: {this.state.roomInfo[0].guestLimit}</p>
+          <p>What To Expect: {this.state.roomInfo[0].description}</p>
+          <p>Host: {this.state.roomInfo[0].User.firstName}</p>
+          <p>Guests Per Night: {this.state.roomInfo[0].guestLimit}</p>
         </div>
 
         <div>
@@ -65,6 +75,7 @@ const RoomDetails = React.createClass({
             <input type='text' placeholder='Zip Code'></input>
             <input type="submit" value="Search" />
           </form>
+            <ul>{this.state.nearbyFood}</ul>
         </div>
       </div>
     )}
