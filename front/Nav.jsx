daf39c9d309e15f6c4a87b896f8b308f945c9e35
michael <michaeldinnall@gmail.com>
2017-01-22 16:31:50 -0500

    cleaned up navBar

--- front/Nav.jsx
+++ front/Nav.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-// import './style/Nav.css';
-// import {connect} from 'react-redux'; 
-// import * as userActions from './reducers/userActions.js' 
 
-
-{/*NAVIGATION BAR*/}
 var Navbar = React.createClass({
   handleLogout: function(e){
     e.preventDefault()
@@ -14,73 +9,34 @@ var Navbar = React.createClass({
       type: 'POST'
     })
     .then(()=>{
-    this.props.logout() //action from userActions
+    this.props.logout() 
     })
   },
-
-
-
-
-
-
-  render: function() {
+   render: function() {
     return (
     <div>
-
-       { /*LOGO*/}
-      <nav className="navMid">
+         <nav className="navMid">
           <div className="navMid">
             <a href="/" className="logoWidth">
         <Link to='/'><img className='logo' src={require('./images/havenHOME.png')} /></Link>
-
             </a>
           </div>
       </nav>
-
-
-      { /*NAV TABS*/}
-      <nav className="navMid">
+      
+       <nav className="navMid">
         <div className="navlist">
-
-          { /*Collect the nav links, forms, and other content for toggling */}
-          <div className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="linetext"><Link to="/home">Home</Link></li> 
+           <div className="nav navbar-nav">
+            <ul className="nav navbar-nav"> 
               <li className="linetext"><Link to="/volunteer-login">Login Host</Link></li> 
-              <li className="linetext"><Link to="/guest-login">Login Guest</Link></li> 
-
-
-{/*
-
-  .header-nav-button.mod-join {
-    min-width: 160px;
-}
-             {this.props.userState.user ?
-                <li className="linetext"><a href='#' onClick={this.handleLogout}>Log Out</a></li> :
-                <li className="linetext"><Link to="/login">Login</Link></li>
-              }
-              {! this.props.userState.user ?
-                <li className="linetext"><Link to="/signup">Sign Up</Link></li> :
-                null
-              }              
-
-*/}
- 
+              <li className="linetext"><Link to="/guest-login">Login Guest</Link></li>
             </ul>
-
-          </div>{ /*/.navbar-collapse */}
-        </div>{ /*/.container-fluid */}
+            </div>
+        </div>
       </nav>
-
-    </div>
+      </div>
     )
   }
 })
-
-// const mapStoreToProps = store => (
-//   {userState: store.userReducer}
-// )
-// export default connect(mapStoreToProps, userActions)(Navbar); //connects: mapStoreToProps is global state in the props
 
 export default Navbar;
 
